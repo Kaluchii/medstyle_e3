a91b14cbb28e5be6db669881b0f7efc277bd2b47
Kaluchii <mr.kaluchii@mail.ru>
2018-06-19 09:54:17 +0600

    Update layout.blade.php

--- resources/views/front/layout.blade.php
+++ resources/views/front/layout.blade.php
@@ -6,7 +6,6 @@
 	<meta name="cmsmagazine" content="8595dcaa5a4b96a7f4b87b77b13368cb" />
     <link rel="shortcut icon" href="/img/fav.png" type="image/png">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="robots" content="index, follow" />
     @yield('meta')
     @include('front.scripts')
     @include('front.styles')
