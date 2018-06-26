37fb230cd578af6ae2cae34ed599b6dce8cea4e6
Kaluchii <mr.kaluchii@mail.ru>
2018-06-26 23:15:27 +0600

    Specs change

--- resources/views/front/specialists/specialist_item.blade.php
+++ resources/views/front/specialists/specialist_item.blade.php
@@ -1,4 +1,4 @@
-<div class="col-1-4">
+<div class="specialists__item">
     <a href="/specialist/{{$spec->slug}}" class="specialist">
         <div class="specialist-photo"><img src="{{$spec->img->link}}?{{$spec->img->cache_index}}" alt="{{$spec->img->alt}}" class="spec"></div>
         <p class="spec-name"><span class="name">{{$spec->spec_name}}</span></p>
