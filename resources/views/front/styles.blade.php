5d69f360a185137ebacac4607d2669550e38d97a
Kaluchii <mr.kaluchii@mail.ru>
2018-07-02 11:46:09 +0600

    Update styles.blade.php

--- resources/views/front/styles.blade.php
+++ resources/views/front/styles.blade.php
@@ -1,4 +1,4 @@
 @section('styles')
-<link rel="stylesheet" href="/css/style.css?10">
-<link rel="stylesheet" href="/css/mobile.css?10">
+<link rel="stylesheet" href="/css/style.css?11">
+<link rel="stylesheet" href="/css/mobile.css?11">
 @endsection
