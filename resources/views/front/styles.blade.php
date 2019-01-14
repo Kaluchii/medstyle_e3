1ec30da716da11d9c258cde7e3e0d9a38a16e980
kaluchii <mr.kaluchii@mail.ru>
2019-01-14 12:31:14 +0600

    Revert face to main page

--- resources/views/front/styles.blade.php
+++ resources/views/front/styles.blade.php
@@ -1,4 +1,4 @@
 @section('styles')
-<link rel="stylesheet" href="/css/style.css?18">
+<link rel="stylesheet" href="/css/style.css?17">
 <link rel="stylesheet" href="/css/mobile.css?17">
 @endsection
