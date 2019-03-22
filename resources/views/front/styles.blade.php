ac1c0591bd884cf3e7f0db7bd685012bffc7705e
Kaluchii <mr.kaluchii@mail.ru>
2019-03-22 11:33:20 +0600

    Fix video layout

--- resources/views/front/styles.blade.php
+++ resources/views/front/styles.blade.php
@@ -1,4 +1,4 @@
 @section('styles')
-<link rel="stylesheet" href="/css/style.css?18">
+<link rel="stylesheet" href="/css/style.css?19">
 <link rel="stylesheet" href="/css/mobile.css?18">
 @endsection
