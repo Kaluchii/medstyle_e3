7f5d925d7995de236d7650b480ae84fc4eac9b58
RustamYussupov <you@example.com>
2019-10-03 11:59:46 +0600

    Youtube video optimization

--- resources/views/front/index/blocks/new-video.blade.php
+++ resources/views/front/index/blocks/new-video.blade.php
@@ -3,11 +3,8 @@
         <h2 class="new-video__title title--l1">Новое видео</h2>
         <div class="new-video__video-wrap">
             <div class="new-video__main-wrap js_video_open">
-                <div class="new-video__main-video-wrap">
-                    <iframe class="new-video__video js_video_play js_video" style="width: 100%; height: 100%"
-                            src="https://www.youtube.com/embed/@if(preg_match("/v=([^&]*)\?/", $videos->video_link . '?', $url)){{ $url[1] }}@endif?rel=0&showinfo=0" frameborder="0"
-                            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-                            allowfullscreen></iframe>
+                <div class="new-video__main-video-wrap js_">
+                    <div class="new-video__video-url js_video_url" data-url="https://www.youtube.com/embed/@if(preg_match("/v=([^&]*)\?/", $videos->video_link . '?', $url)){{ $url[1] }}@endif?rel=0&showinfo=0&autoplay=1"></div>
                     <div class="new-video__poster-block js_poster_hide">
                         <img class="new-video__video-poster" src="{{$videos->img->link}}?{{$videos->img->cache_index}}"
                              alt="{{$videos->video_name}}">
@@ -20,10 +17,7 @@
                 <ul class="new-video__list">
                     @foreach($videos->videos_group as $item)
                         <li class="new-video__item js_video_open">
-                            <iframe class="new-video__video js_video_play js_video" style="width: 100%; height: 100%"
-                                    src="https://www.youtube.com/embed/@if(preg_match("/v=([^&]*)\?/", $item->video_link . '?', $url)){{ $url[1] }}@endif?rel=0&showinfo=0" frameborder="0"
-                                    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-                                    allowfullscreen></iframe>
+                            <div class="new-video__video-url js_video_url" data-url="https://www.youtube.com/embed/@if(preg_match("/v=([^&]*)\?/", $item->video_link . '?', $url)){{ $url[1] }}@endif?rel=0&showinfo=0&autoplay=1"></div>
                             <div class="new-video__poster-block js_poster_hide">
                                 <img class="new-video__video-poster"
                                      src="{{$item->img->link}}?{{$item->img->cache_index}}" alt="{{$item->video_name}}">
